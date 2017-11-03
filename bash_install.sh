72a0d20e96f67561ad0fe9e360451808140b1856
Camilo100 <camilo.cristia1@gmail.com>
2017-11-03 20:05:35 -0300

    Bash

---
+++ bash_install.sh
@@ -0,0 +1,58 @@
+#Instalation process for tradingStation on ubuntu 14.04
+sudo add-apt-repository ppa:fkrull/deadsnakes-python2.7 #must be python 2.9+
+sudo apt-get update
+sudo apt-get upgrade 									#python 2.9+
+sudo pip install numpy									#numpy
+sudo apt-get install libatlas-base-dev gfortran
+sudo pip install scipy									#scipy
+sudo pip install pandas									#pandas
+sudo apt-get install python-matplotlib 					#must be version 1.4+
+sudo apt-get install libfreetype6-dev
+sudo pip install --upgrade python-matplotlib 			#matplotlib
+sudo pip install quandl									#quandl
+#Not checked:
+	#sudo pip install statsmodels
+	#sudo pip install scikit-learn
+
+
+
+#Instalation for ubuntu 16.06
+
+if [python -V = Python 2.7.12] #arreglar esto
+
+sudo apt-get install python-pip
+
+if [pip --version =  8.1.1]
+
+
+#Github
+sudo apt-get install git
+mkdir /qubit-signalboy
+cd /qubit-signalbot
+git clone https://github.com/Camilo100/qubit-signalbot
+cd /qubit-signalbot
+
+
+
+#Drive
+sudo apt install gnome-control-center gnome-online-accounts
+
+#Ir a settings => online accounts => 
+#https://www.howtogeek.com/196635/an-official-google-drive-for-linux-is-here-sort-of-maybe-this-is-all-well-ever-get/
+#user: info@qcap.com.ar
+#pass: QuantumCM2017
+
+
+#Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
+
+#Pip instalations
+pip install pandas
+pip install pandas-datareader
+pip install matplotlib
+ 
+#Sublime text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
---
+++ test.sh
@@ -0,0 +1,4 @@
+if [$1 = $!]
+then	
+	echo hola
+fi
